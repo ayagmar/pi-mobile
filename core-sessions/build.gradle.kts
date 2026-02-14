7e6e72f60da9c19647f1ef51dc18806bd19354b5
Abdeslam Yassine Agmar <abdeslamagmar@gmail.com>
2026-02-14 21:01:36 +0100

    perf(sessions): implement cached indexed session repository

--- core-sessions/build.gradle.kts
+++ core-sessions/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.kotlin.plugin.serialization")
 }
 
 kotlin {
@@ -7,5 +8,9 @@ kotlin {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+
     testImplementation(kotlin("test"))
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.1")
 }
