3604829620ec9a4ece2dc3865e1d6f9df5215311
Abdeslam Yassine Agmar <abdeslamagmar@gmail.com>
2026-02-15 12:48:45 +0000

    perf(chat): limit prism grammar bundle to required languages

--- app/src/main/java/com/ayagmar/pimobile/ui/chat/DiffPrismBundle.kt
+++ app/src/main/java/com/ayagmar/pimobile/ui/chat/DiffPrismBundle.kt
@@ -3,7 +3,24 @@ package com.ayagmar.pimobile.ui.chat
 import io.noties.prism4j.annotations.PrismBundle
 
 @PrismBundle(
-    includeAll = true,
+    include = [
+        "kotlin",
+        "java",
+        "javascript",
+        "json",
+        "markdown",
+        "markup",
+        "makefile",
+        "python",
+        "go",
+        "swift",
+        "c",
+        "cpp",
+        "csharp",
+        "css",
+        "sql",
+        "yaml",
+    ],
     grammarLocatorClassName = ".DiffPrism4jGrammarLocator",
 )
 class DiffPrismBundle
